b70cd233e782fb865a1b2c080806a9a811c2fe92
tztsai <1213815284@qq.com>
2024-11-28 17:02:24 +0000

    remove starter templates

--- frontend-vue/src/router/index.ts
+++ frontend-vue/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
 
 const router = createRouter({
@@ -9,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'main',
       component: GameView,
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView,
     },
     {
       path: '/about',
